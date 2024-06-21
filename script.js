a13006d07c9ff5c26f70bf6befbb19da8df85f96
SonaliKumari1605 <160021428+SonaliKumari1605@users.noreply.github.com>
2024-06-21 11:12:46 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,27 @@
+let blue=document.getElementById("Lightblue");
+let green=document.getElementById("Lightgreen");
+let yellow=document.getElementById("LightYellow");
+let lavender=document.getElementById("Lavender");
+let pink=document.getElementById("Lightpink");
+let gray=document.getElementById("Lightgray");
+function changecolor(color){
+    document.body.style.background=color;
+}
+function bluecolor(){
+    changecolor('lightblue');
+} 
+function greencolor(){
+    changecolor('lightgreen');
+} 
+function yellowcolor(){
+    changecolor('lightyellow');
+} 
+function lavendercolor(){
+    changecolor('lavender');
+} 
+function pinkcolor(){
+    changecolor('lightpink');
+} 
+function greycolor(){
+    changecolor('lightgray');
+} 
